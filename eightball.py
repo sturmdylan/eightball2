7bc5cc01286e9b213b637dff2680ea4bc05e0d26
sturmdylan <sturm_dylan@student.mahoningctc.com>
2021-10-04 08:17:30 -0400

    first commit

---
+++ eightball.py
@@ -0,0 +1,31 @@
+name = "dylan"
+
+question = "Will I win the lottery"
+
+answer = ""
+
+import random
+
+random_number = random.randint(1, 9)
+
+if random_number == 1:
+  answer = "yes - definitley."
+elif random_number == 2:
+  answer = "It is decidedly so."
+elif random_number == 3:
+  answer = "without a doubt"
+elif random_number == 4:
+  answer = "Reply hazy, try again."
+elif random_number == 5:
+  answer = "ask again later"
+elif random_number == 6:
+  answer = "Better not tell you now"
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+print(name + " asks: " + question)
+
+print("Magic 8-ball's answer: " + answer)
