61ee84fc26e2a36a067af1b38a12efa7e4cfe49b
sturmdylan <sturm_dylan@student.mahoningctc.com>
2022-01-07 08:11:24 -0500

    update

---
+++ Dictionaries.py
@@ -0,0 +1,10 @@
+ex_dict = {"a": "anteater", "b": "bear", "c": "crawfish"}
+ 
+ex_dict.keys()
+# dict_keys(["a","b","c"])
+ 
+ex_dict.values()
+# dict_values(["anteater", "bear", "crawfish"])
+ 
+ex_dict.items()
+# dict_items([("a","anteater"),("b","bear"),("c","crawfish")])
---
+++ mini-capstone.py
@@ -0,0 +1,20 @@
+pip install pytube3
+from pytube import YouTube
+
+link = input(“Enter the link: “)
+yt = YouTube(link)
+
+#Title of video
+print(“Title: “,yt.title)
+
+#Number of views of video
+print(“Number of views: “,yt.views)
+
+#Length of the video
+print(“Length of video: “,yt.length,”seconds”)
+
+#Description of video
+print("Description: ",yt.description)
+
+#Rating
+print("Ratings: ",yt.rating)
